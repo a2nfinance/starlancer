a15c64770f37bd009e27e07e38e5f4b07eda24bb
a2nfinance <levi@a2n.finance>
2024-01-10 18:05:09 +0700

    implement is_allow_changing_status

--- frontend/src/hooks/useRoles.ts
+++ frontend/src/hooks/useRoles.ts
@@ -1,8 +1,11 @@
-import { UserRoles } from "@/controller/dao/daoDetailSlice";
+import { ProjectRoles, Task, UserRoles } from "@/controller/dao/daoDetailSlice";
 
 export const useRoles = () => {
     const isAllowApply = (userRoles: UserRoles) => {
         return (!userRoles.is_member && !userRoles.is_job_manager && !userRoles.is_treasury_manager && !userRoles.is_project_manager && !userRoles.is_member_manager)
     };
-    return { isAllowApply };
+    const isAllowChangingStatus = (projectRoles: ProjectRoles, task: Task, address: string) => {
+        return (projectRoles.is_code_reviewer || projectRoles.is_task_manager || task.creator === address);
+    }
+    return { isAllowApply, isAllowChangingStatus };
 };
